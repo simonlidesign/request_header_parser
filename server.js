6dd90b0a57f51144cdc9c76bef8608f364745de3
Simon <liyujunsimon@gmail.com>
2017-01-11 05:49:05 +0000

    first

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    var regexpSoftware = /\(([^()]*)\)/;
+    
+    var json = {
+        // http://stackoverflow.com/questions/10849687/express-js-how-to-get-remote-client-address
+        'ipaddress': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        'language': req.headers['accept-language'].substring(0, 5),
+        'software': req.headers['user-agent'].match(regexpSoftware)[1]
+    }
+    res.send(json);
+});
+
+app.listen(8080);
